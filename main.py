eb2ce0194c45a33089debb5dff9697db2ae285e9
Guilherme Nogueira Leite <guilhermenogueiracontato2016@gmail.com>
2026-02-24 15:29:39 -0300

    feat: adicionado mensagem se é menor ou maior de idade

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ idade = int(input("Quantos anos você tem? "))
 
 print(f"\nPrazer, {nome}!")
 print(f"Você já viveu aproximadamente {idade * 365} dias.\n")
+
+if idade < 18:
+    {
+        print("Bora curtir também, mas com responsabilidade!\n\n")
+    }
+
+else:
+    {
+        print("Você é maior de idade. Bora curtir!\n")
+    }
